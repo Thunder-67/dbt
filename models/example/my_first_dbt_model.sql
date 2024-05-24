37623dee746064ddfd1f8d5fb935465b635f36b3
Aman Gupta <amangupta19653@gmail.com>
2024-05-24 07:09:47 +0000

    test

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -11,9 +11,21 @@
 
 with source_data as (
 
-    select 1 as id
-    union all
-    select null as id
+    SELECT CHANNEL,CASE
+        WHEN length(DATE) = 10 AND substr(DATE, 5, 1) = '-' AND substr(DATE, 8, 1) = '-' THEN DATE
+        WHEN length(DATE) = 10 AND substr(DATE, 3, 1) = '-' AND substr(DATE, 6, 1) = '-' 
+        THEN concat(substring(DATE, 7, 4), '-', substring(DATE, 4, 2), '-', substring(DATE, 1, 2))
+        ELSE NULL
+    END AS DATE,
+DIVISION,
+CASE 
+    WHEN SUBSTR(SKU, 1, 3) IN ('ABC', 'DEF', 'GHI') THEN SUBSTR(SKU, 1, 3)
+    ELSE NULL 
+END AS BRAND,
+REGEXP_REPLACE(SUBSTR(SKU, 4, POSITION(' ' IN SKU) - 4), '[^0-9]', '') AS PRODUCT,
+POS_UNITS,
+PARTNER,SALES_ORG,cast(SALES AS FLOAT) AS SALES,UNITS_ON_HAND,STORE FROM CUSTOMER_POS
+
 
 )
 
