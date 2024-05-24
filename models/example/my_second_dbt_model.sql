b47e3b6b70aad3e27dc25ca98fd3151b2a998cf1
Aman Gupta <amangupta19653@gmail.com>
2024-05-24 02:32:09 +0000

    OKK2

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,7 +1,8 @@
 
 -- Use the `ref` function to select from other models
 {{config(
-    schema='TRANSFORMED_POS')}}
+    materialized="VIEW",
+    schema="TRANSFORMED_POS")}}
 select *
 from {{ ref('my_first_dbt_model') }}
 
